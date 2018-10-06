8ab7be16098f6e0988db9fff33facba1f5c25092
Kein <rvl_beto_aad@hotmail.com>
2018-10-06 01:49:50 -0500

    logo y ajustes responsivos

--- app/src/main/java/com/example/zein/cam/MainActivity.java
+++ app/src/main/java/com/example/zein/cam/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.zein.cam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
@@ -11,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.loadUrl("http://localhost:8080/cam/");
     }
 }
