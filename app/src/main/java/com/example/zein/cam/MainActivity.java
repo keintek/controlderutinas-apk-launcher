30b442600aa0148e8bf323b7df1f27b0fdcc1354
Kein <rvl_beto_aad@hotmail.com>
2018-10-05 21:19:47 -0500

    inicio

---
+++ app/src/main/java/com/example/zein/cam/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.zein.cam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://localhost:8080/cam/");
+    }
+}
